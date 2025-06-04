135a5c58133dee8877b7850d9b0600fefc1c3cbb
defarrel <Defarrel>
2025-06-04 22:53:51 +0700

    Menambahkan future register dan login

--- lib/data/repository/auth_repository.dart
+++ lib/data/repository/auth_repository.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:restapi/data/model/request/auth/login_request_model.dart';
+import 'package:restapi/data/model/request/auth/register_request_model.dart';
 import 'package:restapi/service/service_http_client.dart';
 
 class AuthRepository {
@@ -6,4 +8,8 @@ class AuthRepository {
   final secureStorage = FlutterSecureStorage();
 
   AuthRepository(this._serviceHttpClient);
+
+  Future register(RegisterRequestModel requestModel) async {}
+
+  Future login(LoginRequestModel requestModel) async {}
 }
