2ec082b54029470724f073e148bbddb2227708ba
defarrel <Defarrel>
2025-06-04 22:02:53 +0700

    Mengubah model request

--- lib/data/model/request/admin/admin_profile_request.dart
+++ lib/data/model/request/admin/admin_profile_request.dart
@@ -1,21 +1,15 @@
 import 'dart:convert';
 
 class AdminProfileRequest {
-    final String? name;
+  final String? name;
 
-    AdminProfileRequest({
-        this.name,
-    });
+  AdminProfileRequest({this.name});
 
-    factory AdminProfileRequest.fromJson(String str) => AdminProfileRequest.fromMap(json.decode(str));
 
-    String toJson() => json.encode(toMap());
-
-    factory AdminProfileRequest.fromMap(Map<String, dynamic> json) => AdminProfileRequest(
-        name: json["name"],
-    );
-
-    Map<String, dynamic> toMap() => {
-        "name": name,
+  Map<String, String> toJson() {
+    return {
+      'name': name ?? '',
     };
+  }
 }
+
--- lib/data/model/request/admin/posting_jual_request_model.dart
+++ lib/data/model/request/admin/posting_jual_request_model.dart
@@ -1,33 +1,24 @@
 import 'dart:convert';
 
 class PostingJualRequestModel {
-    final int? burungId;
-    final String? burungType;
-    final int? harga;
-    final String? deskripsi;
+  final int? burungId;
+  final String? burungType;
+  final int? harga;
+  final String? deskripsi;
 
-    PostingJualRequestModel({
-        this.burungId,
-        this.burungType,
-        this.harga,
-        this.deskripsi,
-    });
+  PostingJualRequestModel({
+    this.burungId,
+    this.burungType,
+    this.harga,
+    this.deskripsi,
+  });
 
-    factory PostingJualRequestModel.fromJson(String str) => PostingJualRequestModel.fromMap(json.decode(str));
-
-    String toJson() => json.encode(toMap());
-
-    factory PostingJualRequestModel.fromMap(Map<String, dynamic> json) => PostingJualRequestModel(
-        burungId: json["burung_id"],
-        burungType: json["burung_type"],
-        harga: json["harga"],
-        deskripsi: json["deskripsi"],
-    );
-
-    Map<String, dynamic> toMap() => {
-        "burung_id": burungId,
-        "burung_type": burungType,
-        "harga": harga,
-        "deskripsi": deskripsi,
+  Map<String, String> toJson() {
+    return {
+      'burung_id': burungId?.toString() ?? '',
+      'burung_type': burungType ?? '',
+      'harga': harga?.toString() ?? '',
+      'deskripsi': deskripsi ?? '',
     };
+  }
 }
